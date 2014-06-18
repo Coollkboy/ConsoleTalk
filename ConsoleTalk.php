9bb6e717734ccb33893dd2246d9e485ca9e92f5c
Kedyn <kedynbolds16@yahoo.com>
2014-06-17 19:35:58 -0700

    Update ConsoleTalk.php
    Did some coding

--- ConsoleTalk.php
+++ ConsoleTalk.php
@@ -1,2 +1,14 @@
-
+<?php
+/*
 __PocketMine Plugin__
+name=ConsoleTalk
+version=1.0
+author=Kedyn Bolds
+class=ConsoleTalk
+apiversion=10,11
+*/
+class ConsoleTalk implements Plugin{
+    private $api, $path, $config,
+    
+    public function 
+__construct(ServerAPI $api, = $server = false 
